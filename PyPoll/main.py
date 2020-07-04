d59293161cce2d91df16cfaf5786c69378f28af2
Emilio Bello <emiliobello@Emilios-MacBook-Pro.local>
2020-07-03 20:50:20 -0500

    changes on PyPoll and PyBank done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,16 +4,31 @@ import csv
 
 csvpath = os.path.join('Resources','election_data.csv')
 
-def py_elections(voter_id, candidate):
+def py_elections(csvreader):
 
-    #The total number of votes cast: count id
-    total_votes = len(voter_id)
-    #A complete list of candidates who received votes: count uniques 
-    #candidates = set(candidate)
     
-    from collections import Counter
-    print(Counter(candidate).keys()) # equals to list(set(candidate))
-    print(Counter(candidate).values()) # counts the elements' frequency
+    candidates_dict = {}
+    for row in csvreader:
+        voter_id.append(row[0])
+        candidate.append(row[2])
+
+   
+   
+        candidates_dict = {}
+        for nombre in candidate:
+            if nombre in candidates_dict:
+                candidates_dict[nombre] += 1
+            else:
+                candidates_dict[nombre] = 1
+    
+    for k in candidates_dict:
+        candidato = str(k)
+        pct_votos = "{:.3%}".format(candidates_dict[k]/total_votes)
+        votos = candidates_dict[k]
+        #if candidates_dict[k] == max(candidates_dict[k]):
+        #    winner = str(k)
+    poll_results = f'{candidato}: {pct_votos} ({votos})'
+    
 
     #The percentage of votes each candidate won: for each key, the pct
 
@@ -23,7 +38,10 @@ def py_elections(voter_id, candidate):
     -------------------------
     Total Votes: {total_votes}
     -------------------------
-
+    {poll_results}
+    {candidates_dict}
+    
+    
     -------------------------
     Winner: 
     -------------------------'''
@@ -35,19 +53,15 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
 
-    voter_id = []
-    candidate = []
-    for row in csvreader:
-        voter_id.append(row[0])
-        candidate.append(row[2])
+    py_elections_output = py_elections(csvreader)
     
-    py_elections(voter_id, candidate)
+    print(py_elections_output)
     
     # create path for output file
     #data_output = os.path.join('analysis', 'py_elections.txt')
 
     #with open(data_output, 'w', newline="") as text:
         
-       # text.write(str(py_elections) + '\n')
+       # text.write(py_elections_output)
        # text.close()
 
