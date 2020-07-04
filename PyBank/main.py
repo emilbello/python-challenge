71cb4382ba25c3526dfb60ef9e901223137945fe
Emilio Bello <emiliobello@Emilios-MacBook-Pro.local>
2020-07-03 22:35:07 -0500

    homework submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,50 +4,12 @@ import os
 import csv
 # Set path for file
 csvpath = os.path.join('Resources','budget_data.csv')
-# define the lists that will be used in the function
 
-# define the functions that takes as parameters two lists, date and profit/losses 
-def py_analysis(monthyear, p_and_l):
+# the functions takes csvreader as a parameter and extracts three lists:
+# date, profit/losses, and the change.
+# Change list will allow finding the max and min change val and date indexes
+def py_analysis(csvreader):
     
-    # evaluate the total profit by sum of all values
-    profit_total = sum(p_and_l)
-    # obtain the number of months by getting the lenght of the month list
-    month_total = len(monthyear)
-    # how many times the profit changed if we have X total transactions. Is X - 1, since the first one doesnt count (no change).
-    # obtain the first and last values from the P&L ist with index [0] and [-1]
-    average_change = round(( p_and_l[-1] - p_and_l[0] )/(month_total - 1 ), 2)
-    
-    
-    #p_and_l_previous = 0
-    #change = []
-    #for i in p_and_l:
-    #    val_change = ( p_and_l[i] - p_and_l_previous )
-    #    change.append(val_change)
-    #    p_and_l_previous = p_and_l[i]
-    
-    # obtain the max and min change.
-    greatest_inc = max(change) 
-    greatest_dec = min(change)
-
-    # print results. Obtain the months index by using the index method change.index(greatest_inc/dec)
-    alltext = f'''Financial Analysis
-    ----------------------------
-    Total Months: {month_total} 
-    Total: ${profit_total}
-    Average Change: ${average_change}
-    Greatest Increase in Profits: {monthyear[change.index(greatest_inc)]} (${greatest_inc})
-    Greatest Decrease in Profits: {monthyear[change.index(greatest_dec)]} (${greatest_dec})'''
-    
-    return alltext
-
-# with open read and get lists and functions to work
-with open(csvpath) as csvfile:
-
-    # use csv reader by specifyng the variable that holds the content and delimiter 
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
-
-
     month_year = []
     p_l = []
     change = []
@@ -58,14 +20,48 @@ with open(csvpath) as csvfile:
         change.append(int(row[1])-previous_pl)
         previous_pl = int(row[1])
     
-    py_analysis_output = py_analysis(month_year, p_l)
+    # evaluate the total profit by sum of all values in the P&L list
+    profit_total = sum(p_l)
+    # obtain the number of months by taking the lenght of the month list
+    month_total = len(month_year)
+    # the profit changed all but the first month, since the first
+    # month doesnt count, we use total months - 1
+    # average total shortcut by using the P&L firstlast values (index [0] and [-1])
+    average_change = round(( p_l[-1] - p_l[0] )/(month_total - 1 ), 2)
+            
+    # obtain the max and min change.
+    greatest_inc = max(change) 
+    greatest_dec = min(change)
+
+    # print results. Obtain the months index by using the index method change.index(greatest_inc/dec)
+    alltext = f'''Financial Analysis
+----------------------------
+Total Months: {month_total} 
+Total: ${profit_total} 
+Average Change: ${average_change}
+Greatest Increase in Profits: {month_year[change.index(greatest_inc)]} (${greatest_inc})
+Greatest Decrease in Profits: {month_year[change.index(greatest_dec)]} (${greatest_dec})'''
+    
+    return alltext
+
+# with open read the file path an assing it to csvfile
+with open(csvpath) as csvfile:
+
+    # use csv reader by specifyng the variable that holds the content (csvfile) and delimiter 
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # skip the headers
+    next(csvreader)
+
+    # run the function for the budget data analysis and assign the output to a variable
+    py_analysis_output = py_analysis(csvreader)
+    # print the message to the terminal 
     print(py_analysis_output)
     
     # create path for output file
     data_output = os.path.join('analysis', 'py_analysis.txt')
 
+    # create a write a text file with the analysis.
     with open(data_output, 'w', newline="") as text:
         
         text.write(py_analysis_output)
         text.close()
-    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,3 @@
-# dependencies
 import os
 import csv
 
@@ -6,46 +5,34 @@ csvpath = os.path.join('Resources','election_data.csv')
 
 def py_elections(csvreader):
 
-    
-    candidates_dict = {}
+    total_votes = 0
+    candidates_count = {}
     for row in csvreader:
-        voter_id.append(row[0])
-        candidate.append(row[2])
+        total_votes = total_votes + 1
+        
+        nombre = row[2]
+        if nombre not in candidates_count:
+            candidates_count[nombre] = 1
+        else:
+            candidates_count[nombre] += 1
+    
+    winner = 0
+    winner_name = ''
+    poll_results = ''
+    for k, v in candidates_count.items():
+        poll_results += f'{k}: {"{:.3%}".format(v/total_votes)} ({v}) \n'
+        if v > winner:
+            winner = v
+            winner_name = k
 
-   
-   
-        candidates_dict = {}
-        for nombre in candidate:
-            if nombre in candidates_dict:
-                candidates_dict[nombre] += 1
-            else:
-                candidates_dict[nombre] = 1
-    
-    for k in candidates_dict:
-        candidato = str(k)
-        pct_votos = "{:.3%}".format(candidates_dict[k]/total_votes)
-        votos = candidates_dict[k]
-        #if candidates_dict[k] == max(candidates_dict[k]):
-        #    winner = str(k)
-    poll_results = f'{candidato}: {pct_votos} ({votos})'
-    
-
-    #The percentage of votes each candidate won: for each key, the pct
-
-    #The total number of votes each candidate won: sum by key
-    #The winner of the election based on popular vote.: higher sum.
-    electionstext = f'''Election Results")
-    -------------------------
-    Total Votes: {total_votes}
-    -------------------------
-    {poll_results}
-    {candidates_dict}
-    
-    
-    -------------------------
-    Winner: 
-    -------------------------'''
-    return electionstext
+    alltext = f'''Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------
+{poll_results}-------------------------
+Winner: {winner_name}
+-------------------------'''
+    return alltext
 
 with open(csvpath) as csvfile:
 
@@ -57,11 +44,10 @@ with open(csvpath) as csvfile:
     
     print(py_elections_output)
     
-    # create path for output file
-    #data_output = os.path.join('analysis', 'py_elections.txt')
+    #create path for output file
+    data_output = os.path.join('analysis', 'py_elections.txt')
 
-    #with open(data_output, 'w', newline="") as text:
+    with open(data_output, 'w', newline="") as text:
         
-       # text.write(py_elections_output)
-       # text.close()
-
+       text.write(py_elections_output)
+       text.close()
